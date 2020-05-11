63fed95b39f36e6197caf20692e742ad1d34b65a
Richard Emate <richardemate@gmail.com>
2020-05-11 14:05:41 +0100

    bank app project

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import string
 import random
+import os
 
 home_menu = ["Staff Login", "Close App"]
 login_menu = ["Create new bank account", "Check Account Details", "Logout"]
@@ -11,6 +12,8 @@ def show_menu(menu):
         print(f'Press {option_num} for {i}')
         option_num = option_num + 1
     menu_input = input('Choose an option: ')
+    while not menu_input.isnumeric() or int(menu_input) > len(menu) or int(menu_input) == 0:
+        menu_input = input('You are entering in a wrong value, try again with the options available: ')
     return menu_input
 
 
@@ -27,6 +30,8 @@ password: {user_data[1]}
 email: {user_data[2]}
 name: {user_data[3]}
             ''')
+            f.flush()
+            os.fsync(f.fileno())
             return True
     return False
 
@@ -62,12 +67,38 @@ Account Email: {customer_data[4]}
             ''')
 
 
+def home_menu_exec(menu_choice):
+    if menu_choice == 1:
+        username = input('Enter your username: ')
+        password = input('Enter your password: ')
+        return {
+            "username": username,
+            "password": password
+        }
+    elif menu_choice == 2:
+        print('App ending')
+
+
+def login_menu_exec(menu_choice):
+    login_menu_choice = ''
+    if menu_choice == 1:
+        create_account()
+        login_menu_choice = int(show_menu(login_menu))
+    elif menu_choice == 2:
+        check_account_details()
+        login_menu_choice = int(show_menu(login_menu))
+    while menu_choice == 3:
+        os.remove("session.txt")
+        app_run()
+    if menu_choice != 3:
+        login_menu_exec(login_menu_choice)
+
+
 def app_run():
     home_choice = int(show_menu(home_menu))
     if home_choice == 1:
-        username = input('Enter your username: ')
-        password = input('Enter your password: ')
-        login_status = login(username, password)
+        current_user = home_menu_exec(home_choice)
+        login_status = login(current_user['username'], current_user['password'])
         while not login_status:
             print('You have entered wrong user credentials, please try again')
             username = input('Enter your username: ')
@@ -75,11 +106,9 @@ def app_run():
             login_status = login(username, password)
         if login_status:
             login_menu_choice = int(show_menu(login_menu))
-            while login_menu_choice == 1:
-                create_account()
-                login_menu_choice = int(show_menu(login_menu))
-            if login_menu_choice == 2:
-                check_account_details();
+            login_menu_exec(login_menu_choice)
+    elif home_choice == 2:
+        quit()
 
 
 app_run()
