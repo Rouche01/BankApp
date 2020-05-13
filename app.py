b8e63563f47e1a7c3d09c70e084e57bfe9384ab0
Richard Emate <richardemate@gmail.com>
2020-05-13 11:27:11 +0100

    fixing bugs

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ login_menu = ["Create new bank account", "Check Account Details", "Logout"]
 
 
 def show_menu(menu):
-    option_num = 1
+    option_number = 1
     for i in menu:
-        print(f'Press {option_num} for {i}')
-        option_num = option_num + 1
+        print(f'Press {option_number} for {i}')
+        option_number = option_number + 1
     menu_input = input('Choose an option: ')
     while not menu_input.isnumeric() or int(menu_input) > len(menu) or int(menu_input) == 0:
         menu_input = input('You are entering in a wrong value, try again with the options available: ')
@@ -67,7 +67,7 @@ Account Email: {customer_data[4]}
             ''')
 
 
-def home_menu_exec(menu_choice):
+def home_menu_execute(menu_choice):
     if menu_choice == 1:
         username = input('Enter your username: ')
         password = input('Enter your password: ')
@@ -79,7 +79,7 @@ def home_menu_exec(menu_choice):
         print('App ending')
 
 
-def login_menu_exec(menu_choice):
+def login_menu_execute(menu_choice):
     login_menu_choice = ''
     if menu_choice == 1:
         create_account()
@@ -91,13 +91,13 @@ def login_menu_exec(menu_choice):
         os.remove("session.txt")
         app_run()
     if menu_choice != 3:
-        login_menu_exec(login_menu_choice)
+        login_menu_execute(login_menu_choice)
 
 
 def app_run():
     home_choice = int(show_menu(home_menu))
     if home_choice == 1:
-        current_user = home_menu_exec(home_choice)
+        current_user = home_menu_execute(home_choice)
         login_status = login(current_user['username'], current_user['password'])
         while not login_status:
             print('You have entered wrong user credentials, please try again')
@@ -106,7 +106,7 @@ def app_run():
             login_status = login(username, password)
         if login_status:
             login_menu_choice = int(show_menu(login_menu))
-            login_menu_exec(login_menu_choice)
+            login_menu_execute(login_menu_choice)
     elif home_choice == 2:
         quit()
 
